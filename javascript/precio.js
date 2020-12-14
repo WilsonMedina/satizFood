06371548909edbbaca47cf3bad084a3b2a46909f
Wilson Medina <wilson.89medina@gmail.com>
2020-12-13 21:18:16 -0300

    chorsese(updated): se actualizan forma de mostrar los precios en el font

--- javascript/precio.js
+++ javascript/precio.js
@@ -7,21 +7,28 @@ export default function precio(btnOne, btn, btntwo){
         const $father = e.target.parentElement,
               $hiden = $father.querySelector('.hidden'),
               $hidenOne = $father.querySelector('.hiddenOne'),
-              $precio = $father.nextElementSibling
+              $precio = $father.nextElementSibling,
+              $sale = $precio.nextElementSibling
 
         if(e.target.matches(btnOne)){
             let valor = $hidenOne.value
             $precio.textContent = `$${valor}`
+            $precio.style.display = 'block'
+            $sale.style.display = 'none'
         }
         if(e.target.matches(btn)){
             let valor = $hiden.value / 2 + 5
             $precio.textContent = `$${valor}`
-           
+            $precio.style.display = 'block'
+            $sale.style.display = 'none'
         }
 
         if(e.target.matches(btntwo)){
             let valor = $hiden.value
             $precio.textContent = `$${valor}`
+            $precio.style.display = 'block'
+            $sale.style.display = 'block'
+            
         }
     }) 
 }
